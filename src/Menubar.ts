f7e5a706585a923c00faf7e3a2d7fe3eafc54bc1
Jordan Dearsley <jordan@dearsley.dev>
2022-06-07 21:57:01 -0400

    Checking in

--- src/Menubar.ts
+++ src/Menubar.ts
@@ -301,7 +301,7 @@ export class Menubar extends EventEmitter {
 			this._browserWindow.setVisibleOnAllWorkspaces(true);
 		}
 
-		this._browserWindow.on('close', this.windowClear.bind(this));
+		this._browserWindow.on('closed', this.windowClear.bind(this));
 
 		// If the user explicity set options.index to false, we don't loadURL
 		// https://github.com/maxogden/menubar/issues/255
